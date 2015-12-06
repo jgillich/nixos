1e736de599b407d285f2c201fa1b71297a284602
Jakob Gillich <jakob@gillich.me>
2015-12-06 22:05:52 +0100

    fix dyndns

--- roles/web.nix
+++ roles/web.nix
@@ -5,7 +5,13 @@ in
 {
   environment.systemPackages = with pkgs; [
     python27Packages.docker_compose
+    davfs2
   ];
+
+  #fileSystems."/mnt/box" = {
+  #  device = "https://dav.box.com/dav";
+  #  fsType = "davfs";
+  #};
 
   virtualisation.docker.enable = true;
   virtualisation.libvirtd.enable = true;
@@ -50,6 +56,20 @@ in
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_pass http://127.0.0.1:8112;
           }
+
+        }
+
+        server {
+          listen 443 ssl;
+          server_name git.app.gillich.me;
+          ssl_certificate /root/.lego/certificates/git.app.gillich.me.crt;
+          ssl_certificate_key /root/.lego/certificates/git.app.gillich.me.key;
+
+          location / {
+            proxy_set_header Host $host;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_pass http://127.0.0.1:10080;
+          }
         }
       '';
     };
@@ -68,31 +88,24 @@ in
   systemd.services.dyndns = {
     description = "Dynamic DNS";
     serviceConfig.Type = "oneshot";
-    path = [ pkgs.curl pkgs.bind pkgs.curl ];
+    path = [ pkgs.curl pkgs.bind ];
 
-    # from http://www.whaleblubber.ca/dynamic-cloudflare/
+    # from http://torb.at/cloudflare-dynamic-dns
     script = ''
-      # set your user, token, name server and a comma-separated list of A records
-      CFUSER=${secrets.cloudflare.login}
-      CFTOKEN=${secrets.cloudflare.apiKey}
-      CFNS=charles.ns.cloudflare.com
-      CFHOSTS=apu.sys.gillich.me
+      DOMAIN=apu.sys.gillich.me
+      NEWIP=`dig +short myip.opendns.com @resolver1.opendns.com`
+      CURRENTIP=`dig +short $DOMAIN @resolver1.opendns.com`
 
-      # get your current external IP address
-      CFIP=$(curl -s http://myip.dnsomatic.com/)
-
-      # find out the ip address listed in DNS for the first host in your list
-      CFHOSTIP=$(nslookup $(echo $CFHOSTS | cut -d ',' -f1) $CFNS | grep Address | tail -1 | cut -d ' ' -f2)
-
-      # if your external IP is different from DNS do the update
-      if [ "$CFIP" != "$CFHOSTIP" ]
+      if [ "$NEWIP" = "$CURRENTIP" ]
       then
-        # build the url you need to do the update
-        CFURL="https://www.cloudflare.com/api.html?a=DIUP&hosts=$CFHOSTS&u=$CFUSER&tkn=$CFTOKEN&ip=$CFIP"
-
-        # use curl to do the dynamic update
-        curl -s $CFURL
-        echo "updated ip"
+        echo "IP address unchanged"
+      else
+        curl --cacert /etc/ssl/certs/ca-certificates.crt \
+          -X PUT "https://api.cloudflare.com/client/v4/zones/ca0fc28b0ea163a97ed05ad2bef5d99d/dns_records/234d4c0bdeac610bac6eb9bcc6617e9d" \
+          -H "X-Auth-Email: ${secrets.cloudflare.login}" \
+          -H "X-Auth-Key: ${secrets.cloudflare.apiKey}" \
+          -H "Content-Type: application/json" \
+          --data "{\"type\":\"A\",\"name\":\"$DOMAIN\",\"content\":\"$NEWIP\"}"
       fi
     '';
 
